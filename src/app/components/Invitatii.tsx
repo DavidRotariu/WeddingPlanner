7d509ae241e77e32689781c632917d85d65531ff
David Rotariu <davidrotariu139@gmail.com>
2024-12-13 19:03:06 +0200

    Styled the form

--- src/app/components/Invitatii.tsx
+++ src/app/components/Invitatii.tsx
@@ -1,14 +1,21 @@
-'use client';
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Box, Flex, Title } from '@mantine/core';
+import { Box, Center, Flex, Text, Title } from '@mantine/core';
 import { InvitatiForm } from './InvitatiForm';
 
 export const Invitatii = () => {
     return (
-        <Box h="100vh" miw="full" p="xl">
-            <Title size="100px" pl="xl">
-                Confirmare
-            </Title>
+        <Box mih="100vh" w="full" p="lg" bg="offwhite.1">
+            <Flex direction="column" align="center">
+                <Title size="8rem" py="sm" c="brown.1">
+                    Confirmare
+                </Title>
+                <Text size="1.5rem" py="xs" c="brown.1">
+                    Te așteptăm cu drag!
+                </Text>
+                <Text size="1.5rem" c="brown.1">
+                    Te rugăm să ne anunți decizia ta prin completarea acetsui formular!
+                </Text>
+            </Flex>
             <Flex justify="center">
                 <InvitatiForm />
             </Flex>
--- theme.ts
+++ theme.ts
@@ -7,5 +7,92 @@ export const theme = createTheme({
         fontFamily: '"Lavishly Yours", cursive',
         fontWeight: '400'
     },
-    fontFamily: "'Arapey', 'serif'"
+    fontFamily: "'Arapey', 'serif'",
+    colors: {
+        blue: [
+            '#91A7BB',
+            '#91A7BB',
+            '#91A7BB',
+            '#91A7BB',
+            '#91A7BB',
+            '#91A7BB',
+            '#91A7BB',
+            '#91A7BB',
+            '#91A7BB',
+            '#91A7BB'
+        ],
+        offwhite: [
+            '#FFFAF9',
+            '#FFFAF9',
+            '#FFFAF9',
+            '#FFFAF9',
+            '#FFFAF9',
+            '#FFFAF9',
+            '#FFFAF9',
+            '#FFFAF9',
+            '#FFFAF9',
+            '#FFFAF9'
+        ],
+        brown: [
+            '#666057',
+            '#666057',
+            '#666057',
+            '#666057',
+            '#666057',
+            '#666057',
+            '#666057',
+            '#666057',
+            '#666057',
+            '#666057'
+        ],
+        pink: [
+            '#E5AFAC',
+            '#E5AFAC',
+            '#E5AFAC',
+            '#E5AFAC',
+            '#E5AFAC',
+            '#E5AFAC',
+            '#E5AFAC',
+            '#E5AFAC',
+            '#E5AFAC',
+            '#E5AFAC'
+        ]
+    },
+    primaryColor: 'blue',
+    components: {
+        InputWrapper: {
+            styles: () => ({
+                label: {
+                    fontSize: '1.25rem',
+                    color: '#666057'
+                }
+            })
+        },
+        Input: {
+            styles: () => ({
+                input: {
+                    borderRadius: '8px'
+                }
+            })
+        },
+        Radio: {
+            styles: () => ({
+                // radio: {
+                //     '& input': {
+                //         backgroundColor: '#91A7BB',
+                //         borderColor: '#91A7BB'
+                //     },
+                //     '& input:checked': {
+                //         backgroundColor: 'transparent',
+                //         borderColor: '#91A7BB'
+                //     }
+                // },
+                input: { backgroundColor: '#666057' },
+                label: {
+                    fontSize: '1.25rem',
+                    color: '#666057'
+                }
+            })
+        }
+    }
 });
