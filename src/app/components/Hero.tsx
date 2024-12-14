da53450785f1f86a509a798e252442d090a909f2
David Rotariu <davidrotariu139@gmail.com>
2024-12-14 13:36:43 +0200

    Added hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,11 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { BackgroundImage, Box, Center, Text } from '@mantine/core';
+import { BackgroundImage, Box, Center, Title } from '@mantine/core';
 
 export const Hero = () => {
     return (
         <Box h="100vh" miw="full">
             <BackgroundImage h="100%" src="flowers.png">
-                <Center></Center>
+                <Center h="90vh">
+                    <Title size="10rem" py="sm" c="white">
+                        Ezekiel & Simona
+                    </Title>
+                </Center>
             </BackgroundImage>
         </Box>
     );
