dfcd5ee4c527978281437726baf59194e6a84eaa
David Rotariu <davidrotariu139@gmail.com>
2024-12-14 13:30:11 +0200

    Implement endpoint to add guests

--- src/app/components/InvitatiForm.tsx
+++ src/app/components/InvitatiForm.tsx
@@ -1,17 +1,83 @@
 'use client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { BackgroundImage, Box, Button, Flex, Radio, Text, TextInput, Image } from '@mantine/core';
 import { useState } from 'react';
 
-export const InvitatiForm = () => {
+export const InvitatiForm = ({ setConfirmat }: any) => {
     const [nrOfInvites, setNrOfInvites] = useState(3);
+    const [email, setEmail] = useState('');
+    const [invites, setInvites] = useState(
+        Array.from({ length: nrOfInvites }).map(() => ({ name: '', surname: '', children: false }))
+    );
+
+    // updating the text inputs
+    const handleInputChange = (index: number, field: 'name' | 'surname', value: string) => {
+        const updatedInvites = [...invites];
+        updatedInvites[index][field] = value;
+        setInvites(updatedInvites);
+    };
+    // updating the radio inputs
+    const handleRadioChange = (index: number, value: string) => {
+        const updatedInvites = [...invites];
+        updatedInvites[index].children = value === 'copil';
+        setInvites(updatedInvites);
+    };
+    // add invitees endpoint
+    const handleSubmit = async () => {
+        const requestBody = {
+            email,
+            guests: invites.map((invite) => ({
+                name: invite.name,
+                surname: invite.surname,
+                children: invite.children
+            }))
+        };
+        try {
+            const response = await fetch('https://accused-puffin-dvtech-d86fdbe0.koyeb.app/v1/guests/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestBody)
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to submit the form');
+            }
+            const data = await response.json();
+            console.log('Form submitted successfully:', data);
+            setConfirmat(true);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const isFormValid = () => {
+        return (
+            email.trim() !== '' && invites.every((invite) => invite.name.trim() !== '' && invite.surname.trim() !== '')
+        );
+    };
+
+    const addInvite = () => {
+        setNrOfInvites(nrOfInvites + 1);
+        setInvites([
+            ...invites,
+            { name: '', surname: '', children: false } // Ensure new invite has 'name', 'surname', and 'children' fields initialized
+        ]);
+    };
+
+    const removeInvite = () => {
+        setNrOfInvites(nrOfInvites - 1);
+        setInvites(invites.slice(0, nrOfInvites - 1));
+    };
 
     return (
         <Flex direction="column" gap="lg" w="80%" py="xl">
             <Flex direction="row" gap="md">
                 <Box w="40"></Box>
                 <Box w="50%">
-                    <TextInput label="Email" />
+                    <TextInput label="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </Box>
             </Flex>
 
@@ -19,27 +85,30 @@ export const InvitatiForm = () => {
                 <Flex direction="row" key={index} gap="md" w="full">
                     <Flex w="40" align="end">
                         {index === nrOfInvites - 1 && nrOfInvites > 1 && (
-                            <Button
-                                radius="50"
-                                w="40"
-                                p="0"
-                                variant="subtle"
-                                onClick={() => {
-                                    setNrOfInvites(nrOfInvites - 1);
-                                }}
-                            >
-                                <Image src="minus.svg" radius="sm" alt="minus"></Image>
+                            <Button radius="50" w="40" p="0" variant="subtle" onClick={removeInvite}>
+                                <Image src="minus.svg" radius="sm" alt="minus" />
                             </Button>
                         )}
                     </Flex>
                     <Box w="30%">
-                        <TextInput miw="4/10" label={`Nume ${index + 1}`} />
+                        <TextInput
+                            label={`Name ${index + 1}`}
+                            value={invites[index].name}
+                            onChange={(e) => handleInputChange(index, 'name', e.target.value)}
+                        />
                     </Box>
                     <Box w="30%">
-                        <TextInput label={`Prenume`} />
+                        <TextInput
+                            label={`Surname ${index + 1}`}
+                            value={invites[index].surname}
+                            onChange={(e) => handleInputChange(index, 'surname', e.target.value)}
+                        />
                     </Box>
                     <Flex align="end" py="xs" ml="lg">
-                        <Radio.Group defaultValue={index < 2 ? 'adult' : 'copil'}>
+                        <Radio.Group
+                            value={invites[index].children ? 'copil' : 'adult'}
+                            onChange={(value) => handleRadioChange(index, value)}
+                        >
                             <Flex direction="row" gap="xl">
                                 <Radio value="adult" label="Adult" />
                                 <Radio value="copil" label="Copil" />
@@ -50,19 +119,14 @@ export const InvitatiForm = () => {
             ))}
             <Flex direction="row" gap="md">
                 <Box w="40"></Box>
-                <Button
-                    radius="50"
-                    w="40"
-                    h="40"
-                    p="0"
-                    variant="subtle"
-                    onClick={() => {
-                        setNrOfInvites(nrOfInvites + 1);
-                    }}
-                >
-                    <Image src="plus.svg" radius="sm" alt="plus"></Image>
+                <Button radius="50" w="40" h="40" p="0" variant="subtle" onClick={addInvite}>
+                    <Image src="plus.svg" radius="sm" alt="plus" />
                 </Button>
             </Flex>
+
+            <Button onClick={handleSubmit} disabled={!isFormValid()}>
+                Submit
+            </Button>
         </Flex>
     );
 };
--- src/app/components/Invitatii.tsx
+++ src/app/components/Invitatii.tsx
@@ -1,24 +1,38 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
 import { Box, Center, Flex, Text, Title } from '@mantine/core';
 import { InvitatiForm } from './InvitatiForm';
+import { useState } from 'react';
 
 export const Invitatii = () => {
+    const [confirmat, setConfirmat] = useState(false);
+
     return (
         <Box mih="100vh" w="full" p="lg" bg="offwhite.1">
-            <Flex direction="column" align="center">
-                <Title size="8rem" py="sm" c="brown.1">
-                    Confirmare
-                </Title>
-                <Text size="1.5rem" py="xs" c="brown.1">
-                    Te așteptăm cu drag!
-                </Text>
-                <Text size="1.5rem" c="brown.1">
-                    Te rugăm să ne anunți decizia ta prin completarea acetsui formular!
-                </Text>
-            </Flex>
-            <Flex justify="center">
-                <InvitatiForm />
-            </Flex>
+            {!confirmat ? (
+                <>
+                    <Flex direction="column" align="center">
+                        <Title size="8rem" py="sm" c="brown.1">
+                            Confirmare
+                        </Title>
+                        <Text size="1.5rem" py="xs" c="brown.1">
+                            Te așteptăm cu drag!
+                        </Text>
+                        <Text size="1.5rem" c="brown.1">
+                            Te rugăm să ne anunți decizia ta prin completarea acetsui formular!
+                        </Text>
+                    </Flex>
+                    <Flex justify="center">
+                        <InvitatiForm setConfirmat={setConfirmat} />
+                    </Flex>
+                </>
+            ) : (
+                <Flex justify="center" align="center">
+                    <Title size="8rem" py="sm" c="brown.1">
+                        Multumim pentru Confirmare!
+                    </Title>
+                </Flex>
+            )}
         </Box>
     );
 };
